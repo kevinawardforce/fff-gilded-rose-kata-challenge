3ceb48456e9af1eaecd2757dfba7c01aeca3b5ec
Kevin <kevin@awardforce.com>
2023-08-18 07:55:24 +0800

    Add test for generic functionality

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,10 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+it("decreases expiry and quality", function() {
+  const gildedRose = new Shop([new Item("generic", 10, 10)]);
+  const items = gildedRose.updateQuality();
+  expect(items[0].sellIn).toBe(9);
+  expect(items[0].quality).toBe(9);
 });
+
+
