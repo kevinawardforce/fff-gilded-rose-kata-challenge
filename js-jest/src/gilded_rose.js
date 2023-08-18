ea80c505cc84ae033dc544030e1df6540f8efd49
Alejandro <alejandro@awardforce.com>
2023-08-18 20:14:18 +1000

    Formatting

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,29 +1,29 @@
-
 const {HandlerFactory} = require("./handlers/factory");
 
 class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+    constructor(name, sellIn, quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
 }
 
 class Shop {
-  constructor(items=[]){
-    this.items = items;
-  }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      const handler = (new HandlerFactory).getHandler(this.items[i]);
-      handler.handle(this.items[i]);
+    constructor(items = []) {
+        this.items = items;
     }
 
-    return this.items;
-  }
+    updateQuality() {
+        for (let i = 0; i < this.items.length; i++) {
+            const handler = (new HandlerFactory).getHandler(this.items[i]);
+            handler.handle(this.items[i]);
+        }
+
+        return this.items;
+    }
 }
 
 module.exports = {
-  Item,
-  Shop
+    Item,
+    Shop
 }
