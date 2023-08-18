4751be06615bbebb65de38c18d680f0b39903da5
Alejandro <alejandro@awardforce.com>
2023-08-18 20:12:31 +1000

    Formatting

--- js-jest/src/handlers/backstage_passes.js
+++ js-jest/src/handlers/backstage_passes.js
@@ -1,30 +1,30 @@
 const {DefaultHandler} = require("./default");
 
 class BackstagePasses extends DefaultHandler {
-	canHandle(item) {
-		return item.name === 'Backstage passes to a TAFKAL80ETC concert';
-	}
+    canHandle(item) {
+        return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+    }
 
-	updateQuality(item) {
-		if (item.sellIn < 0) {
-			item.quality = 0;
-			return;
-		}
+    updateQuality(item) {
+        if (item.sellIn < 0) {
+            item.quality = 0;
+            return;
+        }
 
-		if (item.sellIn < 6) {
-			item.quality = this.increaseQuality(item, 3);
-			return;
-		}
+        if (item.sellIn < 6) {
+            item.quality = this.increaseQuality(item, 3);
+            return;
+        }
 
-		if (item.sellIn < 11) {
-			item.quality = this.increaseQuality(item, 2);
-			return;
-		}
+        if (item.sellIn < 11) {
+            item.quality = this.increaseQuality(item, 2);
+            return;
+        }
 
-		item.quality = this.increaseQuality(item);
-	}
+        item.quality = this.increaseQuality(item);
+    }
 }
 
 module.exports = {
-	BackstagePasses
+    BackstagePasses
 }
