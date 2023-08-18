bb8605c102c2bfc93fcbb4ed4a604257c37324c3
Kevin <kevin@awardforce.com>
2023-08-18 08:05:05 +0800

    Add test for the quality degrades twice as fast once sell by date has passed scenario

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -2,9 +2,18 @@ const {Shop, Item} = require("../src/gilded_rose");
 
 it("decreases expiry and quality", function() {
   const gildedRose = new Shop([new Item("generic", 10, 10)]);
+
   const items = gildedRose.updateQuality();
+
   expect(items[0].sellIn).toBe(9);
   expect(items[0].quality).toBe(9);
 });
 
+it("quality degrades twice as fast once sell by date has passed", function() {
+  const gildedRose = new Shop([new Item("generic", 0, 10)]);
 
+  const items = gildedRose.updateQuality();
+
+  expect(items[0].sellIn).toBe(-1);
+  expect(items[0].quality).toBe(8);
+});
